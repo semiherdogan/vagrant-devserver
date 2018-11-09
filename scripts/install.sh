2a164c4ec4a8be6655f3e548e0b981c65207aa16
Semih ERDOGAN <semih.erdogan@vidizayn.com>
2018-11-09 18:43:34 +0300

    use mysql root password

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ apt install curl zip
 
 sh install/apache.sh
 sh install/mysql.sh
+# mysql_secure_installation
 sh install/mongodb.sh
 sh install/redis.sh
 sh install/memcached.sh
@@ -13,7 +14,7 @@ sh install/composer.sh
 
 # SELECT user,authentication_string,plugin,host FROM mysql.user;
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'"
-mysql -u root -e "FLUSH PRIVILEGES"
+mysql -u root -proot -e "FLUSH PRIVILEGES"
 
 a2enmod proxy_fcgi setenvif alias rewrite
 a2enconf php7.2-fpm
