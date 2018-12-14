6686e9d46cc3abc9e397566257c20ce4e5f54f39
Semih ERDOGAN <hasansemiherdogan@gmail.com>
2018-12-14 10:11:42 +0300

    wip

--- scripts/sync_config_files.sh
+++ scripts/sync_config_files.sh
@@ -11,12 +11,38 @@ else
     echo "Apache config file not found."
 fi
 
+# Apache vhost config
+if [ -d /etc/apache2/sites-enabled/ ]
+then
+    rm -rf /etc/apache2/sites-enabled/*
+	cp -r $CURRENT_DIR/../sites-enabled/ /etc/apache2/
+
+    echo "Restarting apache2 ..."
+    systemctl restart apache2
+else
+    echo "Apache sites-enabled folder not exists."
+fi
+
 # Mysql config
 if [ -d "/etc/mysql/mysql.conf.d/" ]
 then
     cp $CURRENT_DIR/../conf/mysqld.cnf /etc/mysql/mysql.conf.d/
+
+    echo "Restarting mysql ..."
+    systemctl restart mysql
 else
     echo "Mysql config folder not exists."
+fi
+
+# Memcached config
+if [ -e /etc/init.d/memcached ]
+then
+    cp $CURRENT_DIR/../conf/memcached.conf /etc/
+
+    echo "Restarting memcached ..."
+    /etc/init.d/memcached restart
+else
+    echo "Memcached not exists."
 fi
 
 # Redis config
@@ -27,27 +53,10 @@ else
     echo "Redis config folder not exists."
 fi
 
-# Memcached config
-cp $CURRENT_DIR/../conf/memcached.conf /etc/
-
-# Apache vhost config
-if [ -d /etc/apache2/sites-enabled/ ]
-then
-    rm -rf /etc/apache2/sites-enabled/*
-	cp -r $CURRENT_DIR/../sites-enabled/ /etc/apache2/
-else
-    echo "Apache sites-enabled folder not exists."
-fi
-
-# Apache config
+# PHP56 config
 if [ -d /etc/apache2/ ]
 then
     cp $CURRENT_DIR/../conf/php56.conf /etc/apache2/
-else
-    echo "Apache folder not exists."
 fi
 
-systemctl restart apache2
-systemctl restart mysql
-/etc/init.d/memcached restart
 # systemctl restart redis
